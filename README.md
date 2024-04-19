f60cabe0acb8e984cda95ce16fc7bf3398d0a06f
azamatyashinov <azamatyashinovv@gmail.com>
2024-04-19 06:26:47 -0700

    ouhsohsp

---
+++ README.md
@@ -0,0 +1,28 @@
+#                   Lesson 37
+
+-  JS Tarixi
+    java Script Brenden Eich (Netscape developr) tomonidan 1995-yilda yaratilgan. Brenden bu dasturlash tilini 10 kun ichida ishlab chiqgan. 
+    JS ning birinchi  nomi "MOCHA" bo'lgan, ikkinchi nomi "LiveScript" bo'lgan oxirgi nomi biz ishlatib kelayotgan nomi ya'ni "JavaScript".
+
+- JS ni ishlatish
+    <!-- Java Scriptda console ga chiqarish -->
+    console.log(...) --- uch nuqta o'rniga chiqarmoqchi bo'lgan o'zgaruvchi kiritiladi.
+    Masalan: 
+        let a;
+        a = 3;
+        console.log(a)
+    <!-- Java Scriptda input va outputlar -->
+    - Output
+        console.log(), alert("...")
+    - Input
+       prompt("How old are you?") 
+    
+    <!-- Math amallar -->
+        + → qo’shish
+        - → ayirish
+        * → ko’paytirish
+        / → bo’lish
+        ** → daraja
+        % → qoldiqli bo’lish
+        Math.floor() → butun qismi
+        Math.abs() → musbat sonlar
---
+++ main.js
@@ -0,0 +1,369 @@
+
+
+/////////////// Darsda qilinadiganlarni qildim////////////
+
+
+           // Begin-1
+
+
+// let a;
+// a = Number(prompt("Kvadrat tomonini kiriting!"))
+// P = 4*a;
+// console.log("kvadrat perimetri "+ P);
+
+
+           // Begin-2
+
+
+
+// let a;
+// a = Number(prompt("Kvadrat tomonini kiriting"));
+// S = a**2;
+// console.log("Kvadrat yuzi = " + S);
+
+
+
+           // Begin-3
+
+// let r;
+// const PI=3.14;
+// r = Number(prompt("Doira radiusini kiriting!"));
+// L=2*PI*r;
+// S=PI*(r**2)
+// console.log("Doira uzunligi = " +  L + ";  " +"Doira yuzi = " + S);
+
+
+
+           // Begin-4
+
+// let a;
+// let b;
+// a = Number(prompt("Manfiy bo'lmagan 1-sonni kiriting__"));
+// b = Number(prompt("Manfiy bo'lmagan 2-sonni kiriting__"));
+// G= (a*b)**(1/2)
+// console.log("Ikki sonning o'rta geometrigi " + G + " ga teng")
+
+
+
+
+            // Begin-5
+
+// let x;
+// x = Number(prompt("y funksiya uchuhn ixtiyoriy x qiymatni kiriting___"));
+// y=3*(x**6)-6*(x**2)-7;
+// console.log(" y funksiyaning " + x + " dagi qiymati " + y + " ga teng")
+
+
+  
+
+
+//////////////////////// UYGA VAZIFA//////////////////
+
+
+              // Begin-1
+
+// let a;
+// let b;
+// a = Number(prompt("To'g'rito'rtburchak birinchi  tomonini kiriting!"))
+// b = Number(prompt("To'g'rito'rtburchak ikkinchi  tomonini kiriting!"))
+// P = 2*(a +b);
+// console.log(" perimetri "+ P + " ga teng");
+
+
+              // Begin-2
+
+// let d;
+// const PI=3.14;
+// d = Number(prompt("Doira deametrini  kiriting!"));
+// L=PI*d;
+// console.log("Doira uzunligi = " +  L );
+
+
+
+              // Begin-3
+
+
+// let a;
+// a = Number(prompt("Kubning tomonini kiriting__"));
+// V = a**3;
+// S= 6*(a**2);
+// console.log("Kubning hajmi " + V  +" ga teng" + "; " + "To'la sirti esa " + S + " ga teng")
+             
+
+              // Begin-4
+
+// let a;
+// let b;
+// let c;
+// a = Number(prompt("Paralelepepidning 1-tomonini kiriting__"));
+// b = Number(prompt("Paralelepepidning 2-tomonini kiriting__"));
+// c = Number(prompt("Paralelepepidning 3-tomonini kiriting__"));
+// V = a*b*c;
+// S=2*(a*b+b*c+c*a);
+// console.log("Parelepepidning hajmi = "+ V + "; To'la sirti = " +S)
+
+
+
+              // Begin-5
+
+// let a;
+// let b;
+// a = Number(prompt("1-sonni kiriting___"));
+// b = Number(prompt("2-sonni kiriting___"));
+// M=(a+b)/2;
+// console.log("Sonlarning o'rta arfmetigi " + M  +" ga teng")
+
+
+
+
+
+        // Begin-6
+
+// let a;
+// let b;
+// let c;
+// a = Number(prompt("To'g'ri burchakli uchburchakning 1-katetini kiriting___"))
+// b = Number(prompt("To'g'ri burchakli uchburchakning 2-katetini kiriting___"))
+// c= (a**2 + b**2)**(1/2)
+// P= a + b + c;
+// console.log("To'g'ri burchakli uchburchakning gipatinuzasi " + c + ", Perimetri" + P + " ga teng.")
+
+
+              // Begin-7
+
+// let r1l;
+// let r2;
+// const PI=3.14;
+// r1=Number(prompt("1-aylana radiusini kiriting___"));
+// r2=Number(prompt("2-aylana radiusini kiriting___"));
+// S1=PI*r1;
+// S2=PI*r2;
+// S = Math.abs(PI*(r1-r2));
+// console.log("1-aylana yuzasi "+ S1 + ", 2-aylana yuzasi "+ S2 + ", ularning ayirmasi " + S + " ga teng")
+
+
+
+              // Begin-8
+
+
+// let L;
+// const PI=3.14;
+// L = Number(prompt("Aylana uzunligini kiriting___"));
+// R=L/(2*PI);
+// S=PI*(R**2);
+// console.log("Aylana radiusi " + R + ", Yzasi " + S + " ga teng")
+
+
+
+             // Begin-9
+
+// let x1;
+// let y1;
+// let x2;
+// let y2;
+// x1 = Number(prompt("Tekislikdagi x1 kordinatani kiriting__"));
+// y1 = Number(prompt("Tekislikdagi y1 kordinatani kiriting__"));
+// x2 = Number(prompt("Tekislikdagi x2 kordinatani kiriting__"));
+// y2 = Number(prompt("Tekislikdagi y2 kordinatani kiriting__"));
+// L=((x1-x2)**2 + (y1-y2)**2)**(1/2)
+// console.log("Ikki nuqta orasidagi masofa " + L + " ga teng")
+
+
+
+              // Begin-10
+// let A;
+// let B;
+// let C;
+// let E;
+// A = Number(prompt(" A sonni  kiriting___"));
+// B= Number(prompt(" A sonni  kiriting___"));
+// C= Number(prompt(" A sonni  kiriting___"));
+// console.log("A = "+ B + ", B = " + C + ", C = " + A )
+
+
+
+             // Integer-1
+
+// let a;
+// a= Number(prompt("Ixtiyoriy uch xonali son kiriting"));
+// birliklar = a % 10;
+// onliklar =  ((a-birliklar)/10)%10;
+// console.log("Birliklar xonasi = " + birliklar + ", o'nliklar xonasi = "+ onliklar)
+
+
+
+             // Integer-2
+
+// let a;
+// a= Number(prompt("Ixtiyoriy uch xonali son kiriting"));
+// birliklar = a % 10;
+// onliklar =  ((a-birliklar)/10)%10;
+// yuzlik = (((a-birliklar)/10-onliklar)/10);
+// yigindi=birliklar + onliklar + yuzlik;
+// console.log("Raqamlari yig'indisi = "+ yigindi)
+
+
+
+
+           // Integer-3
+
+// let a;
+// a= Number(prompt("Ixtiyoriy uch xonali son kiriting"));
+// birliklar = a % 10;
+// onliklar =  ((a-birliklar)/10)%10;
+// yuzliklar = (((a-birliklar)/10-onliklar)/10);
+// teskari=(birliklar*10+onliklar)*10 + yuzliklar
+// console.log("Raqamlari teskari tartibda  = "+ teskari)
+
+
+
+            // Integer-4
+
+// let a;
+// a= Number(prompt("Ixtiyoriy uch xonali son kiriting"));
+// birliklar = a % 10;
+// onliklar =  ((a-birliklar)/10)%10;
+// yuzliklar = (((a-birliklar)/10-onliklar)/10);
+// teskari=(onliklar*10+yuzliklar)*10 + birliklar
+// console.log(" Yuzliklar va onliklar o'rni almashgan holati  = "+ teskari)
+
+
+
+ 
+             // Integer-5
+
+
+// let a;
+// a= Number(prompt("Ixtiyoriy uch xonali son kiriting"));
+// birliklar = a % 10;
+// onliklar =  ((a-birliklar)/10)%10;
+// yuzliklar = (((a-birliklar)/10-onliklar)/10);
+// teskari=(yuzliklar*10+birliklar)*10 + onliklar
+// console.log(" Birliklar va onliklar o'rni almashgan holati  = "+ teskari)
+
+
+// Uyga vazifa tugadi.
+
+// ////////////Qo'shimcha/////////
+
+
+              // Begin-11
+
+
+// let x;
+// x = Number(prompt("y funksiya uchuhn ixtiyoriy x qiymatni kiriting___"));
+// y=4*((x-3)**6)-7*((x-3)**3)+2;
+// console.log(" y funksiyaning " + x + " dagi qiymati " + y + " ga teng")
+
+
+
+
+             // Begin-12
+
+
+// let A;
+// let n;
+// A = Number(prompt("Ixtiyoriy son kiriting__"));
+// n = Number(prompt( A + " Sonni nechinchi darajaga ko'tarishni xohlaysiz__?"));
+// p=A**n;
+// console.log("Javob: "+ p)    
+
+
+
+
+            // Begin-13 
+
+// let Tf;
+// Tf = Number(prompt("Temperaturaning Tf dagi qiymatini kiriting__"));
+// Tc = (Tf-32)*(5/9)
+// console.log("Temperaturaning Tc dagi qiymati = "+ Tc)
+
+
+
+
+           // Begin-14
+
+// let X;
+// let A;
+// let Y;
+// let B;
+// let a, b;
+// X = Number(prompt("Shokolad miqdorini kiriting__" ));
+// A = Number(prompt( X + " kg" + "shokolad narxini kiriting__" ));
+// Y = Number(prompt("Konfet miqdorini kiriting__" ));
+// B = Number(prompt( X + " kg" + "konfet narxini kiriting__" ));
+// a = A/X;
+// b = B/Y;
+// console.log(" 1 kg shokolad = " + a + " so'm" + ", 1 kg kanfet = "+ b+ " so'm")
+
+
+
+
+            // Begin-15
+
+// let X;
+// let A;
+// let Y;
+// let B;
+// X=Number(prompt("Abdulaziz necha kg olma oldi?___"));
+// A = Number(prompt("Olmani narxi qancha edi?___"));
+// Y=Number(prompt("Abdulaziz yana necha kg olma oldi?___"));
+// B = Number(prompt("Olmani narxi qancha edi?___"));
+// jami=A*X+B*Y;
+// console.log("Abdulaziz jami "+ jami+ " so'm savdo qildi")
+
+
+
+            // Integer-6
+
+// let a;
+// a = Number(prompt("999 dan katta ixtiyoriy son kiriting__"));
+// n= Math.floor(a/1000)%10;
+// console.log(a + " sonining minglar xonasidagi raqam "+ n + " ga teng")
+
+
+
+
+              // Integer-7
+
+// let N;
+// N = Number(prompt("Kun boshidab buyon necha sekund o'tdi?___"));
+// m=N/60;
+// console.log("Kunboshidan buyon " + m + " minut o'tgan")
+
+
+
+
+               // Integer-8
+
+// let N;
+// N = Number(prompt("Kun boshidab buyon necha sekund o'tdi?___"));
+// s=N/360;
+// console.log("Kunboshidan buyon " + s + " soat o'tgan")
+
+
+
+
+               // Integer-9
+    
+// let N;
+// N = Number(prompt("Kun boshidab buyon necha sekund o'tdi?___"));
+// m=Math.floor(N/60);
+// s=N%60;
+// console.log("Kunboshidan buyon " + m + " minut " + s +" sekund o'tgan ")
+
+
+
+
+             // Integer-10
+
+
+// let N;
+// N = Number(prompt("Kun boshidab buyon necha sekund o'tdi?___"));
+// h=Math.floor(N/3600);
+// s=N%360;
+// console.log("Kunboshidan buyon " + h + " soat " + s +" sekund o'tgan ")
+
+
+
+// ////////////Qo'shimcha vazifa tugadi////////////
